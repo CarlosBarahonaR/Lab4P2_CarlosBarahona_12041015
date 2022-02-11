def9aba00ce08cd50544f94ff97a3bb296cfc260
CJBarahona <cbarahona@t-developing.com>
2022-02-11 15:59:12 -0600

    Imprimir familias

--- Lab4P2_CarlosBarahona_12041015/src/lab4p2_carlosbarahona_12041015/Lab4P2_CarlosBarahona_12041015.java
+++ Lab4P2_CarlosBarahona_12041015/src/lab4p2_carlosbarahona_12041015/Lab4P2_CarlosBarahona_12041015.java
@@ -139,85 +139,20 @@ public class Lab4P2_CarlosBarahona_12041015 {
                     break;
                 }
                 case 3: {
-                    System.out.println("¿Como desea imprimir?\n"
-                            + "1) Imprimir por posicion de la lista \n"
-                            + "2) Imprimir lista completa \n"
-                            + "3) Imprimir por nombre científico \n"
-                            + "4) Salir");
 
-                    int opcion3 = r.nextInt();
+                    for (int u = 0; u < familias.size(); u++) {
+                        if (familias.get(u) instanceof Familia) {
+                            System.out.println(familias.get(u).toString());
 
-                    while (opcion3 != 4) {
-                        switch (opcion3) {
-                            case 1: {
-                                System.out.println("Ingrese la posicon de la lista del animal que desea imprimir");
-                                int posicion = r.nextInt();
-
-                                try {
-
-                                    System.out.println(animales.get(posicion));
-                                } catch (Exception e) {
-                                    System.out.println("La posición establecida para imprimir no existe.");
-                                }
-                                break;
-                            }
-                            case 2: {
-                                for (int u = 0; u < animales.size(); u++) {
-                                    if (animales.get(u) instanceof Animales) {
-                                        System.out.println(animales.get(u));
-
-                                    }
-                                }
-                                break;
-                            }
-                            case 3: {
-                                System.out.println("Ingrese el nombre cientifico del animal para buscarlo");
-                                String nombreCientifico = r.nextLine();
-                                r.nextLine();
-
-                                int item2 = 0;
-
-                                for (int z = 0; z < animales.size(); z++) {
-                                    String nc = "";
-                                    if (animales.get(z) instanceof Animales) {
-                                        nc = ((Animales) animales.get(z)).getNombreCientifico();
-                                    }
-                                    if (nombreCientifico.equals(nc)) {
-                                        item2 = z;
-                                        z = animales.size();
-                                    } else {
-                                        item2 = 100000;
-                                    }
-                                }
-                                if (item2 <= animales.size()) {
-                                    if (animales.get(item2) instanceof Animales) {
-                                        System.out.println(animales.get(item2));
-                                    }
-                                }
-                                break;
-                            }
                         }
-
-                        break;
                     }
+
+                    break;
+
                 }
 
                 case 4: {
-                    System.out.println("Ingrese la posicon del animal que se alimentara");
-                    int posicion = r.nextInt();
-                    System.out.println("Ingrese la posicon del animal que sera devorado");
-                    int posicion2 = r.nextInt();
-
-                    try {
-
-                        int vida = animales.get(posicion).getVida();
-                        int vida2 = animales.get(posicion2).getVida();
-                        animales.get(posicion).setVida(vida + vida2);
-                        animales.remove(posicion2);
-                    } catch (Exception e) {
-                        System.out.println("Una de las posiciones establecidas no existe.");
-                    }
-
+                  
                     break;
                 }
 
